d4697616fe635622a47ae7e4d1d039e039e4aa14
Martin Westwood <martin.westwood@cts-america.com>
2015-04-08 13:18:09 -0500

    Added development and regression environments, updated developer role

---
+++ environments/development.rb
@@ -0,0 +1,4 @@
+name "development"
+description "Stable environment for testing new development that is ready for QA to test"
+cookbook "RMS", "= 9.4.0"
+cookbook "Jail", "= 9.4.0"
---
+++ environments/regression.rb
@@ -0,0 +1,4 @@
+name "regression"
+description "Stable environment for regression testing"
+cookbook "RMS", "= 9.4.0"
+cookbook "Jail", "= 9.4.0"
