e110c62780f9d37103056ba81b5a73eeb002fd5b
Martin Westwood <martin.westwood@cts-america.com>
2015-05-12 10:45:04 -0500

    Increasing tables maintained in the RMS cookbook

--- cookbooks/RMS/recipes/Arrest_dbo_Arr_ESubmitLog.rb
+++ cookbooks/RMS/recipes/Arrest_dbo_Arr_ESubmitLog.rb
@@ -12,118 +12,77 @@ sql_server_database_table 'Arr_ESubmitLog' do
   action        :create
 end
 
-sql_server_database_column 'Arr_ESubmitLog.UniqueKey' do
+sql_server_database_column 'Arrest.dbo.Arr_ESubmitLog.UniqueKey' do
   connection sql_server_connection_info
-  database_name 'Arrest'
-  table_name    'Arr_ESubmitLog'
-  schema_name   'dbo'
   type          'VARCHAR'
   size          '22'
-  columnname    'UniqueKey'
   null          false
   action        :create
 end
 
-sql_server_database_column 'Arr_ESubmitLog.ARRESTNO' do
+sql_server_database_column 'Arrest.dbo.Arr_ESubmitLog.ARRESTNO' do
   connection sql_server_connection_info
-  database_name 'Arrest'
-  table_name    'Arr_ESubmitLog'
-  schema_name   'dbo'
   type          'VARCHAR'
   size          '15'
-  columnname    'ARRESTNO'
   null          false
   action        :create
 end
 
-sql_server_database_column 'Arr_ESubmitLog.SubmissionType' do
+sql_server_database_column 'Arrest.dbo.Arr_ESubmitLog.SubmissionType' do
   connection sql_server_connection_info
-  database_name 'Arrest'
-  table_name    'Arr_ESubmitLog'
-  schema_name   'dbo'
   type          'VARCHAR'
-  size          '4'
-  columnname    'SubmissionType'
   null          false
   action        :create
 end
 
-sql_server_database_column 'Arr_ESubmitLog.TransmittedAt' do
+sql_server_database_column 'Arrest.dbo.Arr_ESubmitLog.TransmittedAt' do
   connection sql_server_connection_info
-  database_name 'Arrest'
-  table_name    'Arr_ESubmitLog'
-  schema_name   'dbo'
   type          'datetime'
-  columnname    'TransmittedAt'
   null          false
   action        :create
 end
 
-sql_server_database_column 'Arr_ESubmitLog.ReceivedAt' do
+sql_server_database_column 'Arrest.dbo.Arr_ESubmitLog.ReceivedAt' do
   connection sql_server_connection_info
-  database_name 'Arrest'
-  table_name    'Arr_ESubmitLog'
-  schema_name   'dbo'
   type          'datetime'
-  columnname    'ReceivedAt'
   null          true
   action        :create
 end
 
-sql_server_database_column 'Arr_ESubmitLog.ServerResponse' do
+sql_server_database_column 'Arrest.dbo.Arr_ESubmitLog.ServerResponse' do
   connection sql_server_connection_info
-  database_name 'Arrest'
-  table_name    'Arr_ESubmitLog'
-  schema_name   'dbo'
   type          'VARCHAR'
   size          '512'
-  columnname    'ServerResponse'
   null          false
   action        :create
 end
 
-sql_server_database_column 'Arr_ESubmitLog.ExternalDocNum' do
+sql_server_database_column 'Arrest.dbo.Arr_ESubmitLog.ExternalDocNum' do
   connection sql_server_connection_info
-  database_name 'Arrest'
-  table_name    'Arr_ESubmitLog'
-  schema_name   'dbo'
   type          'int'
-  columnname    'ExternalDocNum'
   null          false
   action        :create
 end
 
-sql_server_database_column 'Arr_ESubmitLog.Username' do
+sql_server_database_column 'Arrest.dbo.Arr_ESubmitLog.Username' do
   connection sql_server_connection_info
-  database_name 'Arrest'
-  table_name    'Arr_ESubmitLog'
-  schema_name   'dbo'
   type          'VARCHAR'
   size          '40'
-  columnname    'Username'
   null          true
   action        :create
 end
 
-sql_server_database_column 'Arr_ESubmitLog.XMLInstance' do
+sql_server_database_column 'Arrest.dbo.Arr_ESubmitLog.XMLInstance' do
   connection sql_server_connection_info
-  database_name 'Arrest'
-  table_name    'Arr_ESubmitLog'
-  schema_name   'dbo'
   type          'text'
-  columnname    'XMLInstance'
   null          false
   action        :create
 end
 
-sql_server_database_column 'Arr_ESubmitLog.XMLResponse' do
+sql_server_database_column 'Arrest.dbo.Arr_ESubmitLog.XMLResponse' do
   connection sql_server_connection_info
-  database_name 'Arrest'
-  table_name    'Arr_ESubmitLog'
-  schema_name   'dbo'
   type          'VARCHAR'
   size          'max'
-  columnname    'XMLResponse'
   null          true
   action        :create
 end
